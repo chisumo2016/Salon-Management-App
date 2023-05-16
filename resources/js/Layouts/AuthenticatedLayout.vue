07cbc10a9f4f9b8cc81d42a0229b0868b4d50aa1
bernard chisumo <bchisumo74@gmail.com>
2023-05-16 16:39:12 +0100

    added the tailwindcss layout for admin

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,8 +29,16 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                <NavLink
+                                    :href="route('dashboard')"
+                                    :active="route().current('dashboard')">
                                     Dashboard
+                                </NavLink>
+
+                                <NavLink
+                                    :href="route('admin.index')"
+                                    :active="route().current('admin.index')">
+                                    Admin
                                 </NavLink>
                             </div>
                         </div>
